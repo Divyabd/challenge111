705743b4b091ccff0c5dbcbd900d9938fcc5034e
Divyabd <divyagowda8393@gmail.com>
2021-02-05 17:12:21 +0530

    1st commit

---
+++ Onee/src/one/One.java
@@ -0,0 +1,13 @@
+package one;
+
+public class One {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 1;
+		int b = 2;
+		System.out.println("sum is :" + a);
+	
+	}
+
+}
